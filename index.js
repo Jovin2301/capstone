51f6eb23d2e3f73e94fe4f6ac13b700e735d52f5
Jovin Wong See Xuan <130966829+Jovin2301@users.noreply.github.com>
2024-05-04 16:43:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const axios = require("axios");
 require('dotenv').config();
 
-const token =process.env.TOKEN;
+const token=process.env.TOKEN;
 const mytoken=process.env.MYTOKEN;
 
 const app=express().use(body_parser.json());
@@ -40,8 +40,8 @@ app.post("/webhook", (req,res)=>{
     if (body_para.object){
         if(body_para.entry && 
             body_para.entry[0].changes && 
-            body_para.entry[0].changes[0].value.messages &&
-            body_para.entry[0].changes[0].value.messages[0] 
+            body_para.entry[0].changes[0].value.message &&
+            body_para.entry[0].changes[0].value.message[0] 
             ){
                 let phone_num_id = body.entry[0].changes[0].value.metadata.phone_number_id; //getting the phonee_number_id
                 let from = body.entry[0].changes[0].value.messages[0].from;
