9e80946d8658443282a25b6d83f3cbf2eafc531d
Jovin Wong See Xuan <130966829+Jovin2301@users.noreply.github.com>
2024-05-06 13:31:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -53,22 +53,16 @@ app.post("/webhook", (req, res) => {
     const { metadata, messages } = change.value;
     const { phone_number_id } = metadata;
     const { from, text } = messages[0];
-    console.log('metadata', metadata);
-    console.log("messages:", messages);
+    
     console.log("Phone number ID:", metadata);
     console.log("Sender:", from);
     console.log("Message:", text.body);
     //https://graph.facebook.com/v19.0/me/messages?access_token=${token}`
     //`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`
-    // axios.post(`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`, {
-    //     messaging_product: "whatsapp",
-    //     to: from,
-    //     text: { body: "Hello jovin" + text.body }
-    // })
-    axios.post(`https://graph.facebook.com/v19.0/me/messages?access_token=${token}`, {
-    messaging_type: "RESPONSE",
-    recipient: { phone_number: from },
-    message: { text: "Hello jovin " + text.body }
+    axios.post(`https://graph.facebook.com/v19.0/${metadata}/messages?access_token=${token}`, {
+        messaging_product: "whatsapp",
+        to: from,
+        text: { body: "Hello jovin" + text.body }
     })
     .then(response => {
         console.log("Message sent successfully");
