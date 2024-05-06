99cc0643f2c46b33f0e8b7642c7ff88f01ecf66a
Jovin Wong See Xuan <130966829+Jovin2301@users.noreply.github.com>
2024-05-06 13:32:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -54,12 +54,12 @@ app.post("/webhook", (req, res) => {
     const { phone_number_id } = metadata;
     const { from, text } = messages[0];
     
-    console.log("Phone number ID:", metadata);
+    console.log("Phone number ID:", phone_number_id);
     console.log("Sender:", from);
     console.log("Message:", text.body);
     //https://graph.facebook.com/v19.0/me/messages?access_token=${token}`
     //`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`
-    axios.post(`https://graph.facebook.com/v19.0/${metadata}/messages?access_token=${token}`, {
+    axios.post(`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`, {
         messaging_product: "whatsapp",
         to: from,
         text: { body: "Hello jovin" + text.body }
