cc6f6b50f0f683db652b9eb5e169f8bdcd4495a8
Jovin Wong See Xuan <130966829+Jovin2301@users.noreply.github.com>
2024-05-13 15:05:16 +0800

    customising message sent
    trying to send another kind of message, instead of the default hello_world message

--- index.js
+++ index.js
@@ -51,9 +51,7 @@ app.post("/webhook", (req, res) => {
     }
 
     const { metadata, messages } = change.value;
-    const { phone_number_id } = metadata;
-    console.log("metadata info", phone_number_id);
-    console.log("What is within messages", messages);                                                                                                    
+    const { phone_number_id } = metadata;                                                                                                 
     const { from, text } = messages[0];
     
     console.log("Phone number ID:", phone_number_id);
@@ -66,7 +64,7 @@ app.post("/webhook", (req, res) => {
     axios.post(`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`, {
         messaging_product: "whatsapp",
         to: from,
-        text: { body: "Hello jovin "+ text.body },
+        //text: { body: "Hello jovin "+ text.body },
         type: "template", 
         template: { 
             name: "account_creation_confirmation_2", 
