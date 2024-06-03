a60d73fbcdcdabe3f9c7757437ddb9f9207799c5
Jovin Wong See Xuan <130966829+Jovin2301@users.noreply.github.com>
2024-06-03 22:21:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -60,20 +60,34 @@ app.post("/webhook", (req, res) => {
     //https://graph.facebook.com/v19.0/me/messages?access_token=${token}`
     //`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`
       
-    
-    
-    axios.post(`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`, {
+    const messagePayload = sendTextMessage ? {
         messaging_product: "whatsapp",
         to: from,
-        text: { body: "Hello jovin "+ text.body }
-        // type: "template", 
-        // template: { 
-        //     name: "account_creation_confirmation_2", 
-        //     "language": {
-        //         "code": "en_US"
-        //     }
-        // }
-    })
+        text: { body: "Hello jovin " + text.body }
+    } : {
+        messaging_product: "whatsapp",
+        to: from,
+        type: "template",
+        template: {
+            name: "account_creation_confirmation_2",
+            language: {
+                code: "en_US"
+            }
+        }
+    };
+    
+    axios.post(`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`, messagePayload)
+    //     messaging_product: "whatsapp",
+    //     to: from,
+    //     text: { body: "Hello jovin "+ text.body }
+    //     // type: "template", 
+    //     // template: { 
+    //     //     name: "account_creation_confirmation_2", 
+    //     //     "language": {
+    //     //         "code": "en_US"
+    //     //     }
+    //     // }
+    // })
     .then(response => {
         console.log("Message sent successfully");
         res.sendStatus(200);
