b724df89265a99157ebdac289704af43f3f06463
Jovin Wong See Xuan <130966829+Jovin2301@users.noreply.github.com>
2024-06-03 23:02:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post("/webhook", (req, res) => {
     axios.post(`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${token}`, {
         messaging_product: "whatsapp",
         to: from,
-        text: { body: "Hello jovin " + text.body },
+        // text: { body: "Hello jovin " + text.body },
         text: { 
             type: "template", 
             template: { 
